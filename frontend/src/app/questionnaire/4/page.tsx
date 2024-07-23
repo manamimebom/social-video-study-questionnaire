931e4cec6d82d57acba36734f1b103ff15f18d76
manamimebom <61339660+manamimebom@users.noreply.github.com>
2024-07-24 04:07:02 +0800

    feat(frontend): add all videos and behavior questionnaires' steps for decision process

--- frontend/src/app/questionnaire/4/page.tsx
+++ frontend/src/app/questionnaire/4/page.tsx
@@ -5,6 +5,7 @@ import Introduction from "../../components/Introduction";
 import IntroductionBehavior from "../../components/IntroductionBehavior";
 import Video, { interactionModes, videoTypes } from "../../components/Video";
 import BehaviorQuestionnaire from "../../components/BehaviorQuestionnaire";
+import StillWatchAdsExplanation from "../../components/StillWatchAdsExplanation";
 
 const AdRelevant_ChooseToWatch = () => {
   const adType = videoTypes.AdRelevant;
@@ -27,7 +28,21 @@ const AdRelevant_ChooseToWatch = () => {
       }));
     }
 
-    setStep(step + 1);
+    if (
+      formDataKey &&
+      formDataKey.match(/^decisional_control_[1-4]$/) &&
+      formDataValue === false.toString() // did not watch Ads, skip next step
+    ) {
+      setStep(step + 2);
+    } else if (
+      formDataKey &&
+      formDataKey.match(/^answer_part_a_[1-4]$/) &&
+      [3, 9, 15, 21].includes(step) // finish Ads and Video behavior questionnaire
+    ) {
+      setStep(step + 5);
+    } else {
+      setStep(step + 1);
+    }
   };
 
   const showStep = () => {
@@ -47,11 +62,180 @@ const AdRelevant_ChooseToWatch = () => {
         );
       case 3:
         return (
-          <BehaviorQuestionnaire
+          <BehaviorQuestionnaire // Ads and Video
             onSubmit={handleNextPage}
             formDataKey="answer_part_a_1"
           />
         );
+      case 4:
+        return (
+          <BehaviorQuestionnaire // Video only
+            onSubmit={handleNextPage}
+            showAdsQuestions={false}
+            showVideoQuestions={true}
+            formDataKey="answer_part_a_1"
+          />
+        );
+      case 5:
+        return <StillWatchAdsExplanation onSubmit={handleNextPage} />;
+      case 6:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={videoTypes.PureAd}
+            videoIdx={0}
+            interactionMode={interactionModes.NoInteraction}
+          />
+        );
+      case 7:
+        return (
+          <BehaviorQuestionnaire // Ads only
+            onSubmit={handleNextPage}
+            showAdsQuestions={true}
+            showVideoQuestions={false}
+            oldAnswers={formData["answer_part_a_1"] as Record<string, string>}
+            formDataKey="answer_part_a_1"
+          />
+        );
+      case 8:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={1}
+            interactionMode={interactionMode}
+          />
+        );
+      case 9:
+        return (
+          <BehaviorQuestionnaire // Ads and Video
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_2"
+          />
+        );
+      case 10:
+        return (
+          <BehaviorQuestionnaire // Video only
+            onSubmit={handleNextPage}
+            showAdsQuestions={false}
+            showVideoQuestions={true}
+            formDataKey="answer_part_a_2"
+          />
+        );
+      case 11:
+        return <StillWatchAdsExplanation onSubmit={handleNextPage} />;
+      case 12:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={videoTypes.PureAd}
+            videoIdx={1}
+            interactionMode={interactionModes.NoInteraction}
+          />
+        );
+      case 13:
+        return (
+          <BehaviorQuestionnaire // Ads only
+            onSubmit={handleNextPage}
+            showAdsQuestions={true}
+            showVideoQuestions={false}
+            oldAnswers={formData["answer_part_a_2"] as Record<string, string>}
+            formDataKey="answer_part_a_2"
+          />
+        );
+      case 14:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={2}
+            interactionMode={interactionMode}
+          />
+        );
+      case 15:
+        return (
+          <BehaviorQuestionnaire // Ads and Video
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_3"
+          />
+        );
+      case 16:
+        return (
+          <BehaviorQuestionnaire // Video only
+            onSubmit={handleNextPage}
+            showAdsQuestions={false}
+            showVideoQuestions={true}
+            formDataKey="answer_part_a_3"
+          />
+        );
+      case 17:
+        return <StillWatchAdsExplanation onSubmit={handleNextPage} />;
+      case 18:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={videoTypes.PureAd}
+            videoIdx={2}
+            interactionMode={interactionModes.NoInteraction}
+          />
+        );
+      case 19:
+        return (
+          <BehaviorQuestionnaire // Ads only
+            onSubmit={handleNextPage}
+            showAdsQuestions={true}
+            showVideoQuestions={false}
+            oldAnswers={formData["answer_part_a_3"] as Record<string, string>}
+            formDataKey="answer_part_a_3"
+          />
+        );
+      case 20:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={3}
+            interactionMode={interactionMode}
+          />
+        );
+      case 21:
+        return (
+          <BehaviorQuestionnaire // Ads and Video
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_4"
+          />
+        );
+      case 22:
+        return (
+          <BehaviorQuestionnaire // Video only
+            onSubmit={handleNextPage}
+            showAdsQuestions={false}
+            showVideoQuestions={true}
+            formDataKey="answer_part_a_4"
+          />
+        );
+      case 23:
+        return <StillWatchAdsExplanation onSubmit={handleNextPage} />;
+      case 24:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={videoTypes.PureAd}
+            videoIdx={3}
+            interactionMode={interactionModes.NoInteraction}
+          />
+        );
+      case 25:
+        return (
+          <BehaviorQuestionnaire // Ads only
+            onSubmit={handleNextPage}
+            showAdsQuestions={true}
+            showVideoQuestions={false}
+            oldAnswers={formData["answer_part_a_4"] as Record<string, string>}
+            formDataKey="answer_part_a_4"
+          />
+        );
+
       default:
         return <div>Page Not Found</div>;
     }

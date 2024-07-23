981d4269b301e396e2d43a7fee58243835b8db6b
manamimebom <61339660+manamimebom@users.noreply.github.com>
2024-07-24 02:56:31 +0800

    feat(frontend): add all videos and behavior questionnaires' steps for simple process

--- frontend/src/app/questionnaire/7/page.tsx
+++ frontend/src/app/questionnaire/7/page.tsx
@@ -52,6 +52,54 @@ const AdIrrelevant_SkippableAfter5Sec = () => {
             formDataKey="answer_part_a_1"
           />
         );
+      case 4:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={1}
+            interactionMode={interactionMode}
+          />
+        );
+      case 5:
+        return (
+          <BehaviorQuestionnaire
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_2"
+          />
+        );
+      case 6:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={2}
+            interactionMode={interactionMode}
+          />
+        );
+      case 7:
+        return (
+          <BehaviorQuestionnaire
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_3"
+          />
+        );
+      case 8:
+        return (
+          <Video
+            onSubmit={handleNextPage}
+            videoType={adType}
+            videoIdx={3}
+            interactionMode={interactionMode}
+          />
+        );
+      case 9:
+        return (
+          <BehaviorQuestionnaire
+            onSubmit={handleNextPage}
+            formDataKey="answer_part_a_4"
+          />
+        );
       default:
         return <div>Page Not Found</div>;
     }
